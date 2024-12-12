1a175a9942b0ca999c54143cdbe85d24817eb676
mudasirhussain55 <hmudasir972@gmail.com>
2024-12-13 03:11:54 +0500

    question04

---
+++ question04.dart
@@ -0,0 +1,8 @@
+void main (){
+temperature_celsius = 25
+temperature_fahrenheit = (temperature_celsius * 9 / 5) + 32
+print(f"{temperature_celsius}C in Fahrenheit is {temperature_fahrenheit}F")
+
+
+
+}
